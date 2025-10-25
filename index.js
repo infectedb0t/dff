330d013a8e5a260ca45ad6ed7ba495c70fdeca7f
infectedb0t <uoovourvovo@gmail.com>
2025-10-25 15:54:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     * ====================================================================
     */
     const botsData = [
-        { "id": 1, "name": "CheckSearch", "description": "Бот проверяет ссылку и показывает инфу со скрином.", "link": "https://scanUserProfileBot" },
+        { "id": 1, "name": "CheckSearch", "description": "Бот проверяет ссылку и показывает инфу со скрином.", "link": "https://t.me/scanUserProfileBot" },
         //{ "id": 2, "name": "BOT2", "description": "Описание второго бота, его функционал и ключевые особенности. Постарайтесь кратко и понятно изложить суть, чтобы пользователь сразу понял ценность.", "link": "https://t.me/telegram_bot_2" },
        // { "id": 3, "name": "BOT3", "description": "Текст с описанием третьего бота. Расскажите, чем он отличается от других, какие у него уникальные возможности или для какой аудитории он предназначен.", "link": "https://t.me/telegram_bot_3" },
        // { "id": 4, "name": "BOT4", "description": "Описание четвертого бота.", "link": "https://t.me/telegram_bot_4" },
@@ -230,5 +230,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
 
 
